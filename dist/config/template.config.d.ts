56f216b188e72002db9d09edb778a9348e8e2e7e
Sahoo-tanmay <sahootanmay108@gmail.com>
2025-06-29 22:36:37 +0530

    Delete dist directory

--- dist/config/template.config.d.ts
+++
@@ -1,7 +0,0 @@
-import express from "express";
-declare const startServer: ({ port, app }: {
-    port: number;
-    app: express.Express;
-}) => void;
-export { startServer };
-//# sourceMappingURL=template.config.d.ts.map
